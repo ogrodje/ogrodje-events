7511476f078c176a65b53dc641dfd5b9846b2923
Oto Brglez <otobrglez@gmail.com>
2024-06-10 09:45:08 +0200

    Adding bin folder.

---
+++ bin/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+DOCKER_IMAGE=${DOCKER_IMAGE:-"ghcr.io/ogrodje/ogrodje-events:latest"}
+DOCKER_HOST="ssh://low"
+
+docker rm -f ogrodje-events && \
+docker pull ${DOCKER_IMAGE} && \
+  docker run -d \
+    --name ogrodje-events \
+    -e HYGRAPH_ENDPOINT=${HYGRAPH_ENDPOINT} \
+    -e DATBASE_URL=jdbc:sqlite:/tmp/ogrodje_events.db \
+    -e SYNC_DELAY="1 hour" \
+    -p 3000:7006 \
+    ghcr.io/ogrodje/ogrodje-events:latest
