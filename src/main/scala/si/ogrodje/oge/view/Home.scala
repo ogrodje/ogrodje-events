a745cf96cde50f85b2a2267c948b11b72c2adbd2
Oto Brglez <otobrglez@gmail.com>
2024-07-01 16:13:15 +0200

    Fixing home view.

--- src/main/scala/si/ogrodje/oge/view/Home.scala
+++ src/main/scala/si/ogrodje/oge/view/Home.scala
@@ -7,70 +7,105 @@ import si.ogrodje.oge.model.db.{Event, Meetup}
 import si.ogrodje.oge.repository.{EventsRepository, MeetupsRepository}
 import si.ogrodje.oge.view.Layout.{defaultLayout, renderHtml}
 
+import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 import java.util.{Calendar, Locale}
+import cats.syntax.all.*
+
+enum Section:
+  case ThisWeek()
+  case NextWeek()
+  case Week(weekNumber: Int)
+
+  override def toString: String = this match
+    case ThisWeek() => "Dogodki v tem tednu"
+    case NextWeek() => "Prihodnji teden"
+    case _          => "Prihodnost"
 
 object Home {
   import si.ogrodje.oge.model.MeetupOps.{*, given}
 
-  private val calendar                      = Calendar.getInstance(Locale.of("sl"))
-  private def weekOfYear: Int               = calendar.get(Calendar.WEEK_OF_YEAR)
-  private val isNextWeek: Int => Boolean    = _ == weekOfYear + 1
-  private val isCurrentWeek: Int => Boolean = _ == weekOfYear
+  private val calendar                         = Calendar.getInstance(Locale.of("sl"))
+  private def weekOfYear: Int                  = calendar.get(Calendar.WEEK_OF_YEAR)
+  private val isNextWeek: Int => Boolean       = _ == weekOfYear + 1
+  private val isCurrentWeek: Int => Boolean    = _ == weekOfYear
+  private val dayKey: OffsetDateTime => String =
+    _.withHour(0).withMinute(0).format(DateTimeFormatter.ofPattern("Y-MM-d"))
 
-  private def groupEvents(events: Seq[Event]): Seq[((Int, Boolean, Boolean), Seq[(String, Seq[Event])])] =
-    events
-      .groupBy(_.weekNumber)
+  private def groupEvents(events: Seq[Event]): (
+    List[(Section, List[Event])],
+    List[(Section, List[Event])]
+  ) =
+    events.map { event =>
+      (
+        (event.weekNumber, isCurrentWeek(event.weekNumber), isNextWeek(event.weekNumber)) match {
+          case (_, true, _)       => Section.ThisWeek()
+          case (_, _, true)       => Section.NextWeek()
+          case (weekNumber, _, _) => Section.Week(weekNumber)
+        },
+        dayKey(event.dateTime),
+        event
+      )
+    }
+      .groupBy(_._1)
+      .map((p, x) => p -> x.groupBy(_._2).values.flatMap(_.map(_._3).sortBy(_.dateTime.toInstant)).toList)
       .toList
-      .sortBy(_._1)
-      .map { case (week, events) =>
-        (week, isCurrentWeek(week), isNextWeek(week)) ->
-          events
-            .groupBy(e =>
-              e.dateTime
-                .withHour(0)
-                .withMinute(0)
-                .format(DateTimeFormatter.ofPattern("Y-MM-d"))
-            )
-            .toList
-            .sortBy(_._1)
-            .map((day, events) => day -> events.sortBy(_.dateTime.toInstant))
+      .sortBy(_._1.ordinal)
+      .partition {
+        case (Section.Week(_), _) => false
+        case _                    => true
       }
 
   private def renderEvents(meetupsCount: Long)(events: Seq[Event]): IO[Response[IO]] = renderHtml(
     defaultLayout(
       div(
         cls := "events",
-        groupEvents(events).map { case ((week, isCurrentWeek, isNextWeek), dates) =>
-          div(
-            cls := "week",
-            div(
-              if isCurrentWeek then div(cls := "week-title", "Dogodki v tem tednu")
-              else if isNextWeek then div(cls := "week-title", "Prihodnji teden")
-              else div(cls                    := "other-week")
-            ),
-            div(
-              cls := "events",
-              dates.map { (_, events) =>
+        groupEvents(events)
+          .bimap(
+            _.map { case (section, events) =>
+              div(
+                cls := "week",
                 div(
-                  cls := "date",
-                  events.map { event =>
-                    div(
-                      cls := "event",
-                      div(cls := "event-name", a(href := event.url.toString, event.name)),
-                      div(cls := "meetup-name", event.meetupName),
-                      div(cls := "event-datetime", span(event.humanWhenWhere))
-                    )
-                  }
+                  section.toString,
+                  div(
+                    cls := "events",
+                    events.sortBy(_.dateTime.toInstant).map { event =>
+                      div(
+                        cls := "event",
+                        div(cls := "event-name", a(href := event.url.toString, event.name)),
+                        div(cls := "meetup-name", event.meetupName),
+                        div(cls := "event-datetime", span(event.humanWhenWhere))
+                      )
+                    }
+                  )
                 )
-              }
-            )
+              )
+            },
+            otherEvents => {
+              div(
+                cls := "week",
+                div(
+                  "Prihodnost",
+                  div(
+                    cls := "events",
+                    otherEvents.flatMap(_._2).sortBy(_.dateTime.toInstant).map { event =>
+                      div(
+                        cls := "event",
+                        div(cls := "event-name", a(href := event.url.toString, event.name)),
+                        div(cls := "meetup-name", event.meetupName),
+                        div(cls := "event-datetime", span(event.humanWhenWhere))
+                      )
+                    }
+                  )
+                )
+              ) :: Nil
+            }
           )
-        }
-      ),
-      p(
-        cls := "info-observe",
-        s"Opazujemo $meetupsCount organizacij in meetup-ov."
+          .toList,
+        p(
+          cls := "info-observe",
+          s"Opazujemo $meetupsCount organizacij in meetup-ov."
+        )
       )
     )
   )
